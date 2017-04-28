f0ebcfde28c3d153888ebb9fe9f88338100ba47e
asambrekar11 <asambrek@andrew.cmu.edu>
2017-04-28 11:38:04 -0400

    First commit

---
+++ code/TODOs.m
@@ -0,0 +1,22 @@
+%
+%TODOs
+%->Creating the point cloud and map representation(p,n,c)
+%->Function to compute normal vectors
+%->Function to compute linkage weights
+%->Function to compute the normal confidence 
+%->Function to compute the range values(from point coordinates)
+%->Function to process the point cloud(calling the above functions)
+%->Tracking (Need to make a decision of using existing library like this one: http://www.geo.tuwien.ac.at/downloads/pg/pctools/pctools.html)
+%->Mapping(Need to decide the functions here)
+%->Filtering
+%->Deskew(Need not be done as we move the robot very slowly)
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%%
