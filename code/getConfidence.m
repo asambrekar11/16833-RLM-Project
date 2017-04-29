e9691d35259831e071d5562bfca6d0aa435be90c
Akshay Hinduja <akshay.hinduja208@gmail.com>
2017-04-29 14:28:27 -0400

    Uploaded getConfidence

--- code/getConfidence.m
+++ code/getConfidence.m
@@ -1,5 +1,5 @@
 function [confidence] = getConfidence(Dist,Normals)
 
 theta3 = 0.5;
-confidence = exp(-theta3*asin(((dot(Dist,Normals))./pdist(Dist))^2));
+confidence = exp(-theta3*asin(((dot(Dist,Normals))./norm(Dist))^2));
 
