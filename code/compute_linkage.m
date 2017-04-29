569ebec54ca6c70b0705328f0cdeee9423baf1e6
shreyanskgp <shreyanskgp@gmail.com>
2017-04-29 16:23:00 -0400

    Updated left and right linkages for circular

--- code/compute_linkage.m
+++ code/compute_linkage.m
@@ -4,6 +4,8 @@ function [ pc_linkage ] = compute_linkage( pc_map,pc_range, theta1, theta2)
 
 pc_linkage = zeros(size(pc_map,1),size(pc_map,2),4);
 
+aug_range = [pc_range(:,end) pc_range(:,end-1) pc_range(:,:) pc_range(:,1) pc_range(:,2)];
+
 %Linkages for l, r, u and b
 proj_linkage_l_1 = zeros(size(pc_map,1),size(pc_map,2));
 proj_linkage_l_2 = zeros(size(pc_map,1),size(pc_map,2));
@@ -15,8 +17,12 @@ proj_linkage_b_1 = zeros(size(pc_map,1),size(pc_map,2));
 proj_linkage_b_2 = zeros(size(pc_map,1),size(pc_map,2));
 
 %Right linkage factors
-proj_linkage_r_1(:,2:end-1) = abs(pc_range(:,2:end-1)-pc_range(:,3:end)-(pc_range(:,1:end-2)-pc_range(:,2:end-1)))./(pc_range(:,1:end-2)-pc_range(:,2:end-1));
-proj_linkage_r_2(:,1:end-2) = abs(pc_range(:,1:end-2)-pc_range(:,2:end-1)-(pc_range(:,2:end-1)-pc_range(:,3:end)))./(pc_range(:,2:end-1)-pc_range(:,3:end));
+% proj_linkage_r_1(:,2:end-1) = abs((pc_range(:,2:end-1)-pc_range(:,3:end)-(pc_range(:,1:end-2)-pc_range(:,2:end-1)))./(pc_range(:,1:end-2)-pc_range(:,2:end-1)));
+% proj_linkage_r_2(:,1:end-2) = abs((pc_range(:,1:end-2)-pc_range(:,2:end-1)-(pc_range(:,2:end-1)-pc_range(:,3:end)))./(pc_range(:,2:end-1)-pc_range(:,3:end)));
+
+proj_linkage_r_1 = abs((aug_range(:,3:end-2)-aug_range(:,4:end-1))./(aug_range(:,2:end-3)-aug_range(:,3:end-2))-1);
+proj_linkage_r_2 = abs((aug_range(:,3:end-2)-aug_range(:,4:end-1))./(aug_range(:,4:end-1)-aug_range(:,5:end))-1);
+
 
 sig_r_1 = 0.5 - 0.5*(proj_linkage_r_1-theta1)*theta2./(sqrt(1+(proj_linkage_r_1-theta1).^2*theta2^2));
 sig_r_1 = sig_r_1(:);
@@ -27,8 +33,11 @@ proj_linkage_r_1 = reshape(min(sig_r_1,sig_r_2),size(proj_linkage_r_1));
 proj_linkage_r_1(isnan(proj_linkage_r_1)) = 0;
 
 %Left linkage factors
-proj_linkage_l_1(:,2:end-1) = abs(pc_range(:,2:end-1)-pc_range(:,1:end-2)-(pc_range(:,3:end)-pc_range(:,2:end-1)))./(pc_range(:,3:end)-pc_range(:,2:end-1));
-proj_linkage_l_2(:,3:end) = abs(pc_range(:,3:end)-pc_range(:,2:end-1)-(pc_range(:,2:end-1)-pc_range(:,1:end-2)))./(pc_range(:,2:end-1)-pc_range(:,1:end-2));
+% proj_linkage_l_1(:,2:end-1) = abs(pc_range(:,2:end-1)-pc_range(:,1:end-2)-(pc_range(:,3:end)-pc_range(:,2:end-1)))./(pc_range(:,3:end)-pc_range(:,2:end-1));
+% proj_linkage_l_2(:,3:end) = abs(pc_range(:,3:end)-pc_range(:,2:end-1)-(pc_range(:,2:end-1)-pc_range(:,1:end-2)))./(pc_range(:,2:end-1)-pc_range(:,1:end-2));
+
+proj_linkage_l_1 = abs((aug_range(:,3:end-2)-aug_range(:,2:end-3))./(aug_range(:,4:end-1)-aug_range(:,3:end-2))-1);
+proj_linkage_l_2 = abs((aug_range(:,3:end-2)-aug_range(:,2:end-3))./(aug_range(:,2:end-3)-aug_range(:,1:end-4))-1);
 
 sig_l_1 = 0.5 - 0.5*(proj_linkage_l_1-theta1)*theta2./(sqrt(1+(proj_linkage_l_1-theta1).^2*theta2^2));
 sig_l_1 = sig_l_1(:);
@@ -40,8 +49,11 @@ proj_linkage_l_1(isnan(proj_linkage_l_1)) = 0;
 
 
 %Bottom linkage factors
-proj_linkage_b_1(2:end-1,:) = abs(pc_range(2:end-1,:)-pc_range(3:end,:)-(pc_range(1:end-2,:)-pc_range(2:end-1,:)))./(pc_range(1:end-2,:)-pc_range(2:end-1,:));
-proj_linkage_b_2(1:end-2,:) = abs(pc_range(1:end-2,:)-pc_range(2:end-1,:)-(pc_range(2:end-1,:)-pc_range(3:end,:)))./(pc_range(2:end-1,:)-pc_range(3:end,:));
+% proj_linkage_b_1(2:end-1,:) = abs(pc_range(2:end-1,:)-pc_range(3:end,:)-(pc_range(1:end-2,:)-pc_range(2:end-1,:)))./(pc_range(1:end-2,:)-pc_range(2:end-1,:));
+% proj_linkage_b_2(1:end-2,:) = abs(pc_range(1:end-2,:)-pc_range(2:end-1,:)-(pc_range(2:end-1,:)-pc_range(3:end,:)))./(pc_range(2:end-1,:)-pc_range(3:end,:));
+
+proj_linkage_b_1(2:end-1,:) = abs((pc_range(2:end-1,:)-pc_range(3:end,:))./(pc_range(1:end-2,:)-pc_range(2:end-1,:))-1);
+proj_linkage_b_2(1:end-2,:) = abs((pc_range(1:end-2,:)-pc_range(2:end-1,:))./(pc_range(2:end-1,:)-pc_range(3:end,:))-1);
 
 sig_b_1 = 0.5 - 0.5*(proj_linkage_b_1-theta1)*theta2./(sqrt(1+(proj_linkage_b_1-theta1).^2*theta2^2));
 sig_b_1 = sig_b_1(:);
@@ -52,8 +64,12 @@ proj_linkage_b_1 = reshape(min(sig_b_1,sig_b_2),size(proj_linkage_b_1));
 proj_linkage_b_1(isnan(proj_linkage_b_1)) = 0;
 
 %Up linkage factors
-proj_linkage_u_1(2:end-1,:) = abs(pc_range(2:end-1,:)-pc_range(1:end-2,:)-(pc_range(3:end,:)-pc_range(2:end-1,:)))./(pc_range(3:end,:)-pc_range(2:end-1,:));
-proj_linkage_u_2(3:end,:) = abs(pc_range(3:end,:)-pc_range(2:end-1,:)-(pc_range(2:end-1,:)-pc_range(1:end-2,:)))./(pc_range(2:end-1,:)-pc_range(1:end-2,:));
+% proj_linkage_u_1(2:end-1,:) = abs(pc_range(2:end-1,:)-pc_range(1:end-2,:)-(pc_range(3:end,:)-pc_range(2:end-1,:)))./(pc_range(3:end,:)-pc_range(2:end-1,:));
+% proj_linkage_u_2(3:end,:) = abs(pc_range(3:end,:)-pc_range(2:end-1,:)-(pc_range(2:end-1,:)-pc_range(1:end-2,:)))./(pc_range(2:end-1,:)-pc_range(1:end-2,:));
+
+proj_linkage_u_1(2:end-1,:) = abs((pc_range(2:end-1,:)-pc_range(1:end-2,:))./(pc_range(3:end,:)-pc_range(2:end-1,:))-1);
+proj_linkage_u_2(3:end,:) = abs((pc_range(3:end,:)-pc_range(2:end-1,:))./(pc_range(2:end-1,:)-pc_range(1:end-2,:))-1);
+
 
 sig_u_1 = 0.5 - 0.5*(proj_linkage_u_1-theta1)*theta2./(sqrt(1+(proj_linkage_u_1-theta1).^2*theta2^2));
 sig_u_1 = sig_u_1(:);
